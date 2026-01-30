4f0436d67a11c29fa604bfe1f53d60fa71af2529
Dursun  Yıldız <drsyldz@Dursun-Mac-mini-4.local>
2026-01-30 14:05:43 +0300

    fix

--- Sources/Views/SandBox/SandBoxView.swift
+++ Sources/Views/SandBox/SandBoxView.swift
@@ -153,7 +153,7 @@ private struct SandboxItemRow: View {
 
     @ViewBuilder
     private var icon: some View {
-        Image(item.icon, bundle: .module)
+        Image(item.icon, bundle: HeliosTrace.bundle)
             .resizable()
             .scaledToFit()
             .clipShape(RoundedRectangle(cornerRadius: 4))
